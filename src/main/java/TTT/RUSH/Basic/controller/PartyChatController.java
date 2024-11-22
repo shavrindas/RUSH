56fb86dede045644c4993ac9e33b9766cf5807a4
Choisooomin <ni7304@gmail.com>
2024-11-22 15:30:47 +0900

    채팅 프로그램 추가

---
+++ src/main/java/TTT/RUSH/Basic/controller/PartyChatController.java
@@ -0,0 +1,32 @@
+package TTT.RUSH.Basic.controller;
+
+import TTT.RUSH.JDBC.entity.Party;
+import TTT.RUSH.JDBC.entity.Users;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+public class PartyChatController {
+
+    // Party Chat 페이지로 이동
+    @GetMapping("/partyChatMainPage")
+    public String partyChatMainPage(HttpSession session, Model model) {
+        // 세션에서 현재 사용자와 파티 정보를 가져옴
+        Party currentParty = (Party) session.getAttribute("currentParty");
+        Users currentUser = (Users) session.getAttribute("user");
+
+        // 현재 사용자나 파티 정보가 없는 경우 리다이렉트
+        if (currentParty == null || currentUser == null) {
+            return "redirect:/userPersonalPage";
+        }
+
+        // 모델에 사용자 이름과 현재 파티 정보를 추가
+        model.addAttribute("username", currentUser.getUsername());
+        model.addAttribute("party", currentParty);
+
+        return "partyChatMainPage"; // HTML 파일 이름 변경
+    }
+}
---
+++ src/main/java/TTT/RUSH/Basic/service/ChatWebSocketHandler.java
@@ -0,0 +1,33 @@
+package TTT.RUSH.Basic.service;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+    private static final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession s : sessions) {
+            if (s.isOpen()) {
+                s.sendMessage(new TextMessage(message.getPayload()));
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        sessions.remove(session);
+    }
+}
